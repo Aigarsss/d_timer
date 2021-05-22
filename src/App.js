ea207b5d27188394aa26c976667f4253e5bffaa2
Aigars <aigars.uplejs@gmail.com>
2021-05-22 15:02:52 +0300

    Add dragging functionality

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import './App.css';
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
-  const [time, setTime] = useState({counter: 86395, hours: 0, minutes: 0, seconds: 0});
+  const [time, setTime] = useState({counter: 0, hours: 0, minutes: 0, seconds: 0});
   const [interv, setInterv] = useState();
+  const [locationProps, setLocationProps] = useState({
+    diffX: 0,
+    diffY: 0,
+    dragging: false,
+    styles: {}
+  });
 
+// Timer functionality
   const handleClick = () => {
     if (isRunning) {
       stop();
@@ -26,11 +33,11 @@ function App() {
     setTime({counter: 0, hours: 0, minutes: 0, seconds: 0});
   }
 
-
   let updatedCounter = time.counter;
 
   const run = () => {
 
+    // Check for 24h, then reset
     if (updatedCounter === 86400) {
       stop();
       return;
@@ -40,6 +47,7 @@ function App() {
     const updatedSeconds = Math.floor(updatedCounter % 3600 % 60);
 
     updatedCounter++;
+
     return setTime({
         counter: updatedCounter, 
         hours: updatedHours, 
@@ -48,22 +56,65 @@ function App() {
       })
   }
 
+  // Dragging functionality
+
+  const dragStart = (e) => {
+    // console.log(e.target.parentElement);
+    // console.log(e.currentTarget);
+    setLocationProps({
+      ...locationProps, 
+      diffX: e.screenX - e.currentTarget.getBoundingClientRect().left,
+      diffY: e.screenY - e.currentTarget.getBoundingClientRect().top,
+      dragging: true
+    })
+  }
+
+  const dragging = (e) => {
+    if (locationProps.dragging) {
+      var left = e.screenX - locationProps.diffX;
+      var top = e.screenY - locationProps.diffY;
+
+      setLocationProps({
+        ...locationProps, 
+        styles: {
+          left: left,
+          top: top
+        }
+      })
+    }
+  }
+
+  const dragEnd = () => {
+    setLocationProps({
+      ...locationProps, 
+      dragging: false
+    })
+  }
+
+
   return (
     <>
       <img src={gatis} className="App-logo" alt="logo" />
-      <div className="timerContainer">
-          <div className="timerMove"></div>
-          <Icon running={isRunning} handleClick = {handleClick} />
-          <div className="timerTime">
-            {time.hours < 10 ? "0" + time.hours : time.hours}:
-            {time.minutes < 10 ? "0" + time.minutes : time.minutes}:
-            {time.seconds < 10 ? "0" + time.seconds : time.seconds}
-          </div>
+      <div 
+        className="timerContainer" 
+        style={locationProps.styles}             
+        onMouseDown={dragStart} 
+        onMouseMove={dragging} 
+        onMouseUp={dragEnd} 
+      >
+        <div className="timerMove"></div>
+
+        <Icon running={isRunning} handleClick = {handleClick} />
+
+        <div className="timerTime">
+          {time.hours < 10 ? "0" + time.hours : time.hours}:
+          {time.minutes < 10 ? "0" + time.minutes : time.minutes}:
+          {time.seconds < 10 ? "0" + time.seconds : time.seconds}
+        </div>
       </div>
     </>
   );
 }
-
 
 const Icon = ({running, handleClick}) => {
 
